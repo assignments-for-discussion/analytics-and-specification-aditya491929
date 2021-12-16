70a0be5895de0d4ccd9813fee3daa00cb451f17f
aditya491929 <aditya.malwade@somaiya.edu>
2021-12-16 08:58:49 +0530

    Added suggested functionality to report NaN

--- average.js
+++ average.js
@@ -1,7 +1,11 @@
 
 function average(numbers) {
-  const nums = numbers.filter((num) => !isNaN(num));
-  return nums.reduce((p, c)=> p + c, 0) / nums.length;
+  const filteredNans = numbers.filter((num) => isNaN(num)).length;
+  const filteredNumbers = numbers.filter((num) => !isNaN(num));
+  if (filteredNans > filteredNumbers.length) {
+    return NaN;
+  }
+  return filteredNumbers.reduce((p, c)=> p + c, 0) / filteredNumbers.length;
 }
 
 module.exports = {average};
--- test/average.test.js
+++ test/average.test.js
@@ -14,3 +14,7 @@ it('reports the average as NaN on an empty list', ()=> {
 it('ignores NaN in the input', ()=> {
   expect(average([1, NaN, 2])).to.be.approximately(1.5, 0.01);
 });
+
+it('report NaN if the input has more NaNs than readings', ()=> {
+  expect(average([NaN, 1, NaN, 2, NaN, NaN, 3, NaN, 4, NaN, NaN])).to.be.NaN;
+});
